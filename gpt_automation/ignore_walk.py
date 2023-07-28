25f8f2176d7c874124032cf2a859e8f56cf3ce7e
DESKTOP-JECE0OC\Admin <globql@outlook.com>
2023-07-27 17:02:20 -0700

    ignore walk made efficient

--- gpt_automation/ignore_walk.py
+++ gpt_automation/ignore_walk.py
@@ -3,33 +3,18 @@ import pathlib
 import igittigitt
 import fnmatch
 import re
-import time
-
 class IgnoreMatch:
-    def __init__(self, gitignore_path, pattern_list=None):
+    def __init__(self, gitignore_path):
         self.gitignore_path = gitignore_path
         self.parser = igittigitt.IgnoreParser()
         self.parser.parse_rule_file(gitignore_path)
-        self.patterns = self.compile_patterns(pattern_list) if pattern_list else []
 
     def match(self, path):
         path = pathlib.Path(path)
-        return self.parser.match(path) or self.matches_list_pattern(str(path))
+        return self.parser.match(path)
 
-    @staticmethod
-    def compile_patterns(pattern_list):
-        return [re.compile(fnmatch.translate(pattern.strip())) for pattern in pattern_list]
-
-    def matches_list_pattern(self, file_path):
-        file_path = file_path.replace("\\", "/")
-        file_path = file_path.lstrip("/")
-        for pattern in self.patterns:
-            if pattern.fullmatch(file_path):
-                return True
-        return False
-
-def load_gitignore(gitignore_path, pattern_list=None):
-    return IgnoreMatch(gitignore_path, pattern_list)
+def load_gitignore(gitignore_path):
+    return IgnoreMatch(gitignore_path)
 
 def matches_any_pattern(file_path, matches_collection):
     for match in matches_collection:
@@ -37,50 +22,31 @@ def matches_any_pattern(file_path, matches_collection):
             return True
     return False
 
+def compile_patterns(patterns_list):
+    compiled_patterns = [re.compile(fnmatch.translate(pattern)) for pattern in patterns_list]
+    return compiled_patterns
+def matches_list_pattern( file_path, patterns):
+    file_path = file_path.replace("\\", "/")
+    file_path = file_path.lstrip("/")
+    for pattern in patterns:
+        if pattern.fullmatch(file_path):
+            return True
+    return False
+
+
 def ignore_walk(path, black_list, white_list):
-    start_time = time.time()
 
     gitignores = [file_path for file_path in pathlib.Path(path).rglob('.gitignore')]
-    elapsed_time = time.time() - start_time
-    #print(f"Time taken for creating gitignores: {elapsed_time:.4f} seconds")
 
-    matches_collection = [load_gitignore(g, black_list) for g in gitignores]
-    elapsed_time = time.time() - start_time
-    #print(f"Time taken for creating matches_collection: {elapsed_time:.4f} seconds")
+    black_list_patterns = compile_patterns(black_list)
+    white_list_patterns = compile_patterns(white_list) if white_list else None
 
-    start_time = time.time()
-
-    for dirpath, dirnames, filenames in os.walk(path):
-        dirnames[:] = [d for d in dirnames if not matches_any_pattern(os.path.join(dirpath, d), matches_collection)]
-        filenames = [f for f in filenames if not matches_any_pattern(os.path.join(dirpath, f), matches_collection)]
-
-        # Print the current directory being traversed
-        #print(f"Processing directory: {dirpath}")
-
-        # Print the elapsed time for each directory traversal
-        elapsed_time = time.time() - start_time
-        #print(f"Time taken for directory traversal: {elapsed_time:.4f} seconds")
-        start_time = time.time()
-
-        yield dirpath, dirnames, filenames
-
-    # Print the overall elapsed time for the ignore_walk function
-    elapsed_time = time.time() - start_time
-    #print(f"Total time taken for ignore_walk: {elapsed_time:.4f} seconds")
-
-
-
-"""
-def ignore_walk_hold(path, black_list, white_list):
-
-    gitignores = [file_path for file_path in pathlib.Path(path).rglob('.gitignore')]
 
     matches_collection = [load_gitignore(g) for g in gitignores]
 
     for dirpath, dirnames, filenames in os.walk(path):
         dirnames[:] = [d for d in dirnames if not matches_any_pattern(os.path.join(dirpath, d), matches_collection)
-                        and not matches_list_pattern(os.path.join(dirpath, d), black_list)]
+                        and not matches_list_pattern(os.path.join(dirpath, d), black_list_patterns)]
         filenames = [f for f in filenames if not matches_any_pattern(os.path.join(dirpath, f), matches_collection)
-                        and (not white_list or matches_list_pattern(os.path.join(dirpath, f), white_list))]
+                        and (not white_list or matches_list_pattern(os.path.join(dirpath, f), white_list_patterns))]
         yield dirpath, dirnames, filenames
-"""
