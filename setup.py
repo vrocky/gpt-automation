27bcdef7e222dcd4a5376ba7e0b50e89abd49539
VINIT\vinit <globql@outlook.com>
2024-03-06 23:24:44 +0530

    Updating version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="gpt_automation",
-    version="0.1.6",
+    version="0.1.7",
     packages=find_packages(),
     entry_points={
         "console_scripts": [
