799b0c3d6f794fae1d44dbf7ac5edf6259b8b419
VINIT\vinit <vinitsiriya@gmail.com>
2024-08-15 07:45:00 +0530

    Fixing setup.py to support json file and package resource directory

--- setup.py
+++ setup.py
@@ -1,6 +1,11 @@
 from setuptools import setup, find_packages
 
 setup(
-
-    packages=find_packages()
+    packages=find_packages(),
+    package_data={
+        'gpt_automation': [
+            '**/*.json',  # Include all JSON files in gpt_automation and its subdirectories
+            'resources/*',  # Include all files in the resources directory
+        ]
+    }
 )
